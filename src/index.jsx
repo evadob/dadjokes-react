d7130669641d806117f2fe36ce2dce08f1b53bb6
Eva Dobružská (evadob) <eva.dobruzska@gmail.com>
2023-06-03 12:41:16 +0200

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+
+const App = () => {
+  return <div className="container"></div>;
+};
+
+createRoot(document.querySelector("#app")).render(<App />);
